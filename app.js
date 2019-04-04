b1b0a50ef703d305b4045e36fb66055612d36594
Fortunato Estorgio <contact@estorgio.com>
2019-04-05 00:27:32 +0800

    YelpCamp - Creating Campgrounds

--- app.js
+++ app.js
@@ -7,27 +7,37 @@ app.set('view engine', 'ejs');
 app.use('/static', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const campgrounds = [
+  {
+    name: 'Salmon Creek',
+    image: 'https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg',
+  },
+  {
+    name: 'Granite Hill',
+    image: 'https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg',
+  },
+  {
+    name: 'Mountain Goat\'s Rest',
+    image: 'https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg',
+  },
+];
+
 app.get('/', (req, res) => {
   res.render('landing');
 });
 
 app.get('/campgrounds', (req, res) => {
-  const campgrounds = [
-    {
-      name: 'Salmon Creek',
-      image: 'https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg',
-    },
-    {
-      name: 'Granite Hill',
-      image: 'https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg',
-    },
-    {
-      name: 'Mountain Goat\'s Rest',
-      image: 'https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg',
-    },
-  ];
-
   res.render('campgrounds', { campgrounds });
+});
+
+app.post('/campgrounds', (req, res) => {
+  const { name, image } = req.body;
+  campgrounds.push({ name, image });
+  res.redirect('/campgrounds');
+});
+
+app.get('/campgrounds/new', (req, res) => {
+  res.render('new');
 });
 
 const port = process.env.PORT || 3000;
