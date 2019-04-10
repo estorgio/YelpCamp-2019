a736b6a7010f732225aa0889876bf4bbcb912ba5
Fortunato Estorgio <contact@estorgio.com>
2019-04-11 04:05:04 +0800

    YelpCamp - User Associations - Campground

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,6 +3,14 @@ const Campground = require('../models/campground');
 
 const router = express.Router();
 
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    next();
+    return;
+  }
+  res.redirect('/login');
+}
+
 router.get('/', (req, res, next) => {
   Campground.find({}, (err, campgrounds) => {
     if (err) {
@@ -13,9 +21,20 @@ router.get('/', (req, res, next) => {
   });
 });
 
-router.post('/', (req, res, next) => {
+router.post('/', isLoggedIn, (req, res, next) => {
   const { name, image, description } = req.body;
-  Campground.create({ name, image, description }, (err) => {
+  const author = {
+    // eslint-disable-next-line no-underscore-dangle
+    id: req.user._id,
+    username: req.user.username,
+  };
+  const newCampground = {
+    name,
+    image,
+    description,
+    author,
+  };
+  Campground.create(newCampground, (err) => {
     if (err) {
       next(err);
       return;
@@ -24,7 +43,7 @@ router.post('/', (req, res, next) => {
   });
 });
 
-router.get('/new', (req, res) => {
+router.get('/new', isLoggedIn, (req, res) => {
   res.render('campgrounds/new');
 });
 
